9292c297de56e4a1a3f2a9d9ccd13862b578901c
paulo <paulosilvarl17@gmail.com>
2025-03-19 14:23:25 -0300

    modificando o banco de dados para mysql

--- Database/produto_db.py
+++ Database/produto_db.py
@@ -1,6 +1,22 @@
-from peewee import SqliteDatabase
+from peewee import MySQLDatabase
+from dotenv import load_dotenv
+import os
 
-db = SqliteDatabase("produtos_por_validade.db")
-from peewee import SqliteDatabase
+# Carregar as variáveis de ambiente do arquivo .env
+load_dotenv()
 
-db = SqliteDatabase("produtos_por_validade.db")
+# Obter as variáveis de ambiente
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = int(os.getenv('DB_PORT'))
+
+# Configurar a conexão com o banco de dados
+db = MySQLDatabase(
+    database=db_name,
+    user=db_user,
+    password=db_password,
+    host=db_host,
+    port=db_port
+)
