75b45d733c70248fff48ca03165d1b70956783ab
paulo <paulosilvarl17@gmail.com>
2025-03-25 14:22:20 -0300

    modifiquei alguns arquivos

--- config.py
+++ config.py
@@ -1,37 +1,21 @@
-# from routes import route_home
-# from routes import route_produto
-# from Database import db 
-# from Database import Produto
-# def configure_all(app):
-#     configure_routes(app)
-#     configure_db()
-
-
-# def configure_routes(app):
-#     app.register_blueprint(route_home)
-#     app.register_blueprint(route_produto, url_prefix="/produto")
-    
-
-
-# # Função para configurar o banco de dados
-# def configure_db():
-#     if db.is_closed():
-#         db.connect()
-#     db.create_tables([Produto])
-
-from routes import route_home, route_produto
+from routes import route_home
+from routes import route_produto
 from Database import db 
 from Database import Produto
-
 def configure_all(app):
-    configure_routes(app)
-    configure_db()
+     configure_routes(app)
+     configure_db()
+
 
 def configure_routes(app):
-    app.register_blueprint(route_home)
-    app.register_blueprint(route_produto, url_prefix="/produto")
+     app.register_blueprint(route_home)
+     app.register_blueprint(route_produto, url_prefix="/produto")
+    
 
+#Função para configurar o banco de dados
 def configure_db():
-    if db.is_closed():
-        db.connect()
-    db.create_tables([Produto])
+     if db.is_closed():
+         db.connect()
+     db.create_tables([Produto])
+
+
