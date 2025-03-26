36d811cf39a89a01a10695ec2bcee9379609e976
paulo <paulosilvarl17@gmail.com>
2025-03-26 20:38:02 -0300

    adicionando arquivos

--- config.py
+++ config.py
@@ -1,15 +1,30 @@
+# from routes import route_home, route_produto
+
+
+# def configure_all(app):
+#      configure_routes(app)
+
+
+
+# def configure_routes(app):
+#      app.register_blueprint(route_home)
+#      app.register_blueprint(route_produto, url_prefix="/produto")
+    
+# config.py
 from routes import route_home, route_produto
 
-
 def configure_all(app):
-     configure_routes(app)
-
-
+    # Defina explicitamente a pasta de templates
+    app.template_folder = 'templates'
+    
+    # Configurar rotas
+    configure_routes(app)
 
 def configure_routes(app):
-     app.register_blueprint(route_home)
-     app.register_blueprint(route_produto, url_prefix="/produto")
-    
+    # Registrar os blueprints
+    app.register_blueprint(route_home)
+    app.register_blueprint(route_produto, url_prefix="/produto")
+
 
 
 
